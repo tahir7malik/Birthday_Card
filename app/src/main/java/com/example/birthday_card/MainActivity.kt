f3884e78d9b3f7e1197bb2a56cea30aa1cebb6bf
tahir7malik <tahir.malik296@gmail.com>
2024-08-14 17:09:56 +0530

    Added background photo for birthday card app, made alignments and added padding to components

--- app/src/main/java/com/example/birthday_card/MainActivity.kt
+++ app/src/main/java/com/example/birthday_card/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -11,8 +14,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.birthday_card.ui.theme.Birthday_CardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,29 +29,54 @@ class MainActivity : ComponentActivity() {
         setContent {
             Birthday_CardTheme {
                 // A surface container using the 'background' color from the theme
-                Surface (
+                Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    GreetingText(
+                        message = "Happy Birthday Eli!",
+                        from = "From Tahir",
+                        modifier = Modifier.padding(8.dp)
+                    )
                 }
             }
         }
     }
 }
+/*
+        UI elements in Android apps use two different units of measurement:
+        1. density-independent pixels (DP), which you use later for the layout, and
+        2. scalable pixels (SP).
 
+        Note: By default, the SP unit is the same size as the DP unit, but it
+        resizes based on the user's preferred text size under phone settings.
+*/
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    // Text composable passing in a text message as named argument
-    Text (
-        text = message
-    )
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column( // Column is composable component for setting parent child hierarchy
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
+        Text( // Text composable passing in a text message as named argument
+            text = message,
+            fontSize = 100.sp, // The scalable pixels (SP) is a unit of measure for the font size
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     Birthday_CardTheme {
-        GreetingText(message = "Happy Birthday Eli!")
+        GreetingText(message = "Happy Birthday Eli!", from = "From Tahir")
     }
 }
