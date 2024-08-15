ce6a56ec37e6a98ef4d5cb4dd1dd6738212376e3
tahir7malik <tahir.malik296@gmail.com>
2024-08-15 07:10:19 +0530

    added image in our design build but text not visible

--- app/src/main/java/com/example/birthday_card/MainActivity.kt
+++ app/src/main/java/com/example/birthday_card/MainActivity.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -73,10 +73,28 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     }
 }
 
+/* Resources can be accessed with resource IDs that are generated in your project's R class.
+   Syntax: R.drawable.graphic
+    {
+             R = auto generated R class,
+      drawable = subdirectory in res folder
+       graphic = Resource ID (mostly same as filename)
+    }
+*/
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     Birthday_CardTheme {
-        GreetingText(message = "Happy Birthday Eli!", from = "From Tahir")
+        //GreetingText(message = "Happy Birthday Eli!", from = "From Tahir")
+        GreetingImage(message = "Happy Birthday Eli!", from = "From Tahir", modifier = Modifier)
     }
 }
